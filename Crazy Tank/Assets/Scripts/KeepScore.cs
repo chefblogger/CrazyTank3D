8136a4001374ff849f545526affef03fda8b3342
chefblogger <70014924+chefblogger@users.noreply.github.com>
2020-09-12 17:46:59 +0200

    V1

---
+++ Crazy Tank/Assets/Scripts/KeepScore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepScore : MonoBehaviour
+{
+
+        public static int Score = 0;
+   
+    void OnGUI(){
+       GUI.skin.box.fontSize = 30; 
+        GUI.Box(new Rect(0, 0, 300, 400), "Dein Score ist:\n" + Score + " Punkte");
+
+        
+
+        if (Score >= 120){
+            
+            Application.LoadLevel("Scene01");
+        }
+    }
+
+    
+    
+
+}
